ad96939e4f973e84a4635f784db7b03ddf3ae404
Javier Marsicano <javier.marsicano@ix.co>
2021-10-02 17:42:28 -0300

    Implement basic player
    - Integrate view binding
    - Add player components for standard video

--- app/src/main/java/com/javiermarsicano/immersiveplayer/MainActivity.kt
+++ app/src/main/java/com/javiermarsicano/immersiveplayer/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.javiermarsicano.immersiveplayer
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.javiermarsicano.immersiveplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var player: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        playerView = binding.playerView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initializePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    private fun initializePlayer() {
+        if (player == null) {
+            player = SimpleExoPlayer.Builder(this).build()
+        }
+
+        val uri = Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4")
+        val mediaSource = buildMediaSource(uri)
+        player?.setMediaSource(mediaSource)
+        player?.prepare()
+
+        playerView.player = player
+        playerView.onResume()
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val mediaItem = MediaItem.Builder()
+            .setUri(uri)
+            .build()
+        val dataSourceFactory = DefaultDataSourceFactory(this, "immersive-player")
+        // Create a media source using the supplied URI
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mediaItem)
+    }
+
+    private fun releasePlayer() {
+        playerView.onPause()
+        player?.release()
+        player = null
     }
 }
